9e69424b3477625eb57acf2095ed5680eb35bdf0
Patricia Campbell <pcampbell@dawsoncollege.qc.ca>
2020-09-14 21:07:10 -0400

    re-org

---
+++ lectures/Lecture5.java
@@ -0,0 +1,5 @@
+public class Lecture5 {
+  public static void main(String[] args) {
+    System.out.println("Integer values 1/2 + 1/2 = " + (1/2 + 1/2));
+  }
+}
---
+++ lectures/lecture6/Lecture6Scanner.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Lecture5Scanner {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    
+    // read in a floating point number
+    
+    // read in an integer
+    System.out.print("What is your name: ");
+    String name = input.nextLine();
+    
+    System.out.println("how many fingers do you have?");
+    int fingers = input.nextInt();
+    
+    System.out.print("What is the USD exchange for a CAD: ");
+    double  dollars = input.nextDouble();
+   
+    System.out.println("\n\nHello " + name);
+    System.out.println("you have " + fingers + " fingers");
+    System.out.println("1.00$ CAD is worth  " + dollars + "$ US");
+  }
+}
