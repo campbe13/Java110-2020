cc0e9653a75909004cdb75e1c2613cb396e83a66
Patricia Campbell <pcampbell@dawsoncollege.qc.ca>
2020-09-22 13:43:21 -0400

    remove extra file

--- labs/lab6/Demo.java
+++
@@ -1,9 +0,0 @@
- double x = -b / 2 * a; 
- // eval to x = (-b/2) * a  // wrong for our purposes
- 
- double x = -b / (2 * a);
- 
- 
- 5 * 6.4 -> conv to double
- 6 - 4 -> int
- 6.0 - 4 -> conv to double
